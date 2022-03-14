36fdc77a41f123f58b87ec3f643f7c3eb1b918e8
Emily Martinek <emily.martinek12@gmail.com>
2022-03-14 08:48:21 +0100

    create show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,20 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
   def show
-    @tasks
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- app/views/tasks/view.rb
+++
@@ -1,7 +0,0 @@
-class View
-  def display(tasks)
-    tasks.each_with_index | task, index | {
-      puts "#{index}. #{task}"
-    }
-  end
-end
